0be919a6a6f50ce71a8572a2dc8192ff28e1dc0b
Gabriel Antonia <Gabriel.Antonia@AccentureFederal.com>
2021-01-22 09:42:02 -0600

    added age field

--- src/com/gabriel/model/User.java
+++ src/com/gabriel/model/User.java
@@ -2,10 +2,12 @@ package com.gabriel.model;
 
 public class User {
 	private String name;
+	public int age;
 
 	public String getName() {
 		return name;
 	}
+	
 
 	public void setName(String name) {
 		this.name = name;
