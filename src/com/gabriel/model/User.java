f6fe3b366d65fd1af603a7246532e3f73700973b
Gabriel Antonia <Gabriel.Antonia@AccentureFederal.com>
2021-01-22 09:36:22 -0600

    added getters/setters

--- src/com/gabriel/model/User.java
+++ src/com/gabriel/model/User.java
@@ -1,5 +1,14 @@
 package com.gabriel.model;
 
 public class User {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
